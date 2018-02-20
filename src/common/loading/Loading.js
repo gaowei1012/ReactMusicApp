b480e37bf6fd55c66219e1222348cb3dd695aba3
gmw-zjw <gaomingwei2017@163.com>
2018-02-20 18:00:17 +0800

    loading commnet

---
+++ src/common/loading/Loading.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import LoadingImg from './loading.gif';
+
+import './loadding.styl';
+
+class Loading extends React.Component {
+    render() {
+        let displayStyle = this.props.show = true ? {display: ''} : {display: 'none'};
+        return (
+            <div className="loading-container">
+                <div className="loading-wrapper">
+                    <img src={LoadingImg} width="18px" height="18px" alt={loading} />
+                    <div className="loading-title">
+                        {this.props.title}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Loading;

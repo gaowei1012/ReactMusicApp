1f8a6eefc178d7aaf6d825c13238c74a056fa23c
gmw-zjw <gaomingwei2017@163.com>
2018-02-22 20:49:56 +0800

    player commit

--- src/components/play/Player.js
+++ src/components/play/Player.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import {Song} from '../../model/song';
+import { Song } from '../../model/song';
+import Progress from './Progress';
 
-import "./Progress";
+import "./player.styl";
 
 class Player extends React.Component {
     constructor(props) {
@@ -31,6 +32,20 @@ class Player extends React.Component {
     }
 
     render() {
+
+        let currentIndex = this.props.currentIndex;
+        
+         //从redux中获取当前播放歌曲
+        if (this.props.currentSong && this.props.currentSong.url) {
+            // 当歌曲发生变化时
+            if (this.currentSong.id !== this.currentSong.id) {
+                this.currentSong = this.props.currentSong;
+                this.audioDOM.src = this.currentSong.url;
+                // 加载资源， ios 调用此方法
+                this.audioDOM.load();
+            }
+        }
+
         let song = this.currentSong;
 
         let playBg = song.img ? song.img : require("../../assets/imgs/play_bg.jpg");
@@ -41,7 +56,78 @@ class Player extends React.Component {
         song.playStatus = this.state.playStatus;
 
         return (
-            <div>player ...</div>
+            <div className="player-container">
+                <CSSTransition in={this.props.showStatus} timeout={300} classNames="player-rotate"
+                    onEnter={() => {
+                        this.playerDOM.style.display = "block";
+                    }}
+                    onExited={() => {
+                        this.playerDOM.style.display = "none";
+                    }}>
+                    <div className="player" ref="player">
+                        <div className="header">
+                            <span className="header-back" onClick={this.hidePlayer}>
+                                <i className="icon-back"></i>
+                            </span>
+                            <div className="header-title">
+                                {song.name}
+                            </div>
+                        </div>
+                        <div className="singer-top">
+                            <div className="singer">
+                                <div className="singer-name">{song.singer}</div>
+                            </div>
+                        </div>
+                        <div className="singer-middle">
+                            <div className="singer-img" ref="singerImg">
+                                <img src={playBg} alt={song.name} onLoad={
+                                    (e) => {
+                                        /*图片加载完成后设置背景，防止图片加载过慢导致没有背景*/
+                                        this.playerBgDOM.style.backgroundImage = `url("${playBg}")`;
+                                    }
+                                } />
+                            </div>
+                        </div>
+                        <div className="singer-bottom">
+                            <div className="controller-wrapper">
+                                <div className="progress-wrapper">
+                                    <span className="current-time">{getTime(this.state.currentTime)}</span>
+                                    <div className="play-progress">
+                                        <Progress progress={this.state.playProgress}
+                                            onDrag={this.handleDrag}
+                                            onDragEnd={this.handleDragEnd} />
+                                    </div>
+                                    <span className="total-time">{getTime(song.duration)}</span>
+                                </div>
+                                <div className="play-wrapper">
+                                    <div className="play-model-button" onClick={this.changePlayMode}>
+                                        <i className={"icon-" + this.playModes[this.state.currentPlayMode] + "-play"}></i>
+                                    </div>
+                                    <div className="previous-button" onClick={this.previous}>
+                                        <i className="icon-previous"></i>
+                                    </div>
+                                    <div className="play-button" onClick={this.playOrPause}>
+                                        <i className={playButtonClass}></i>
+                                    </div>
+                                    <div className="next-button" onClick={this.next}>
+                                        <i className="icon-next"></i>
+                                    </div>
+                                    <div className="play-list-button" onClick={this.showPlayList}>
+                                        <i className="icon-play-list"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="player-bg" ref="playerBg"></div>
+                        <audio ref="audio"></audio>
+                    </div>
+                </CSSTransition>
+                <MiniPlayer song={song} progress={this.state.playProgress}
+                    playOrPause={this.playOrPause}
+                    next={this.next}
+                    showStatus={this.props.showStatus}
+                    showMiniPlayer={this.showPlayer} />
+            </div>
         );
     }
 }

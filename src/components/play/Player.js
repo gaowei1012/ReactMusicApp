d2e206beb59da8930314bc04bd7fc32cfd2ea531
gmw-zjw <gaomingwei2017@163.com>
2018-02-22 10:02:04 +0800

    player commit

--- src/components/play/Player.js
+++ src/components/play/Player.js
@@ -1,9 +1,48 @@
-import React from "react"
+import React from "react";
+import ReactDOM from 'react-dom';
+import {Song} from '../../model/song';
+
+import "./Progress";
 
 class Player extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.currentSong = new Song(0, "", "", 0, "", "");
+        this.currentIndex = 0;
+
+        // 播放模式 list-列表 single-单曲shuffle-随机
+        this.playModes = ["list", "single", "shuffle"];
+
+        // 初始化状态
+        this.state = {
+            currentTime: 0,
+            playProgress: 0,
+            playStatus: false,
+            currentPlayMode: 0
+        };
+    }
+
+    componentDidMount() {
+        this.audioDOM = ReactDOM.findDOMNode(this.refs.audio);
+        this.singerImgDOM = ReactDOM.findDOMNode(this.refs.singerImg);
+        this.playerDOM = ReactDOM.findDOMNode(this.refs.player);
+        this.playerBgDOM = ReactDOM.findDOMNode(this.refs.playerBg);
+    }
+
     render() {
-        console.log(this.props.currentSong);
-        console.log(this.props.playSongs);
+        let song = this.currentSong;
+
+        let playBg = song.img ? song.img : require("../../assets/imgs/play_bg.jpg");
+
+        // 播放按钮样式
+        let playButtonClass = this.state.playStatus === true ? "icon-pause" : "icon-play";
+
+        song.playStatus = this.state.playStatus;
+
+        return (
+            <div>player ...</div>
+        );
     }
 }
 
--- src/components/play/Progress.js
+++ src/components/play/Progress.js
@@ -7,22 +7,75 @@ import './progress.styl';
 class Progress extends React.Component {
 
     componentDidUpdate() {
-        
+        // 组件更新后重新获取进度条总宽度
+        if (!this.progressBarWidth) {
+            this.progressBarWidth = ReactDOM.findDOMNode(thid.refs.progressBar).offsetWidth;
+        }
+
     }
 
     componentDidMount() {
-
+        // progress
         let propressBarDOM = ReactDOM.findDOMNode(this.refs.progressBar);
         let progressDOM = ReactDOM.findDOMNode(this.refs.progress);
         let progressBtnDOM = ReactDOM.findDOMNode(this.refs.progressBtn);
         this.progressBarWidth = progressBtnDOM.offsetWidth;
 
+        // 移动端拖拽
+        let {disableButton, disableDrag, onDragStart, onDragEnd} = this.props;
+        if (disableButton !== true && disableDrag !==true) {
+            // 触摸开始位置
+            let downX = 0;
+            // 按钮letf值
+            let buttonLeft = 0;
+
+            progressBtnDOM.addEventListener("touchstart", (e) => {
+                let touch = e.touches[0];
+                downX = touch.clientX;
+                buttonLeft = parseInt(touch.target.style.left, 10);
+                // 开始记录
+                if (onDragStart) {
+                    onDragStart();
+                }
+            });
+
+            progressBtnDOM.addEventListener("touchmove", (e) => {
+                e.preventDefault();
+
+                let touch = e.touches[0];
+                let diffX = touch.clientX -downX;
+
+                let btnLeft = buttonLeft + diffX;
+                if (btnLeft > progressDOM.offsetWidth) {
+                    btnLeft = progressBtnDOM.offsetWidth; 
+                } else if (btnLeft < 0) {
+                    btnLeft = 0;
+                }
+
+                // 设置按钮left 值
+                touch.target.style.left = btnLeft + 'px';
+                // 设置进度width值
+                progressDOM.style.width = btnLeft / this.progressBarWidth * 100 + '%' ;
+
+                if (onDrag) {
+                    onDrag(btnLeft / this.progressBarWidth);
+                }
+            });
+
+            progressBtnDOM.addEventListener("touchend", (e) => {
+                if (onDragEnd) {
+                    onDragEnd();
+                }
+            });
+
+        }
+
     }
 
     render() {
-        let {progress, disableButton} = this.props;
+        let { progress, disableButton } = this.props;
         if (!progress) progress = 0;
-        
+
         // left 按钮的值
         let progressButtonOffsetLeft = 0;
         if (this.progressBarWidth) {
@@ -30,8 +83,13 @@ class Progress extends React.Component {
         }
         return (
             <div className="progress-bar" ref="propressBar">
-                <div className="progress" style={{width: "20%"}} ref="progress"></div>
-                <div className="progress-buttom" style={{left: "70px"}} ref="progressBtn"></div>
+                <div className="progress-load"></div>
+                <div className="progress" style={{ width: `${progress * 100}%` }} ref="progress"></div>
+                {
+                        disableButton === true ? "" :
+                        <div className="progress-buttom" style={{ left: progressButtonOffsetLeft}} ref="progressBtn"></div>
+
+                }
             </div>
         );
     }
